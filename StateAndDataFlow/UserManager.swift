852fe6bf9004db1a0e3d82227fb05f3af5935077
Dymitr12 <dk39fra@gmail.com>
2022-05-18 22:11:25 +0300

    Start

---
+++ StateAndDataFlow/UserManager.swift
@@ -0,0 +1,18 @@
+//
+//  UserManager.swift
+//  StateAndDataFlow
+//
+//  Created by Dymitr on 18.05.2022.
+//
+
+import Foundation
+//import Combine
+
+final class UserManager: ObservableObject {
+    
+    @Published var isregistered = false
+    var name = ""
+    
+}
+
+
